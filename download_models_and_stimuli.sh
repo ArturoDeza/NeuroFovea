05a904a2416f602e4e46eba9569474db03c48f1e
Arturo <arturodeza@gmail.com>
2019-05-01 19:59:49 -0400

    Create download_models_and_stimuli.sh

---
+++ download_models_and_stimuli.sh
@@ -0,0 +1,6 @@
+wget https://drive.google.com/file/d/1FjEgSqfq_MaxGuOJVwjKL-deOOa-iSpL/view?usp=sharing
+unzip models.zip
+https://drive.google.com/file/d/1STEbiHFdSWA8QVEyWX9_bPkzH8JJkyEV/view?usp=sharing
+unzip Refinement.zip
+wget https://drive.google.com/file/d/1GSIZIN1zCEF_rcLFIeWxabp-gaiFA7xZ/view?usp=sharing
+unzip Stimuli_Color.zip
